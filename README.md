bdcb055ea58e2a2f365bca6b69209f4e83e59891
liyinchi_gitlab <liyinchi@qq.com>
2024-01-27 22:28:09 +0800

    esp32遥控小车代码

---
+++ README.md
@@ -0,0 +1,176 @@
+#  基于ESP32 开发板（MCU）遥控小车
+
+嵌入式单片机ESP32Arduino 遥控小车（蓝牙版）
+
+<img src="" width="300" height="300">
+
+
+## 材料
+
+ESP32主板1个、步进器1个（电机驱动）、3.7V电池1个（18000mAh）、车身架1个、2个轮胎和2个小马达、跳线若干。
+
+### 小车主板
+
+<img width="449" alt="image" src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/2fcd5151-e3a0-4d4b-9fbe-824e7a37c865">
+
+
+### 一、ESP32 NodeMCU 单片机
+
+<img src="" width="300" height="400">
+
+拓展板
+
+<img src="" width="300" height="400">
+
+备注：ESP32引脚数量有限，使用拓展板来引出更多引脚
+
+#### 引脚图
+
+<img src="" width="400" height="300">
+
+#### Arduino IDE 安装ESP32Audio库
+
+1. 打开Arduino IDE。
+
+3. 在菜单栏中，选择Sketch -> Include Library -> Manage Libraries...。
+
+5. 在弹出的库管理器窗口中，输入ESP32库到搜索框中。
+
+7. 在搜索结果中找到ESP32，点击Install按钮进行安装。
+
+
+#### 选择开发板
+
+1. 打开Arduino IDE。
+
+2. 在菜单栏中，选择工具 -> 开发板 -> 选择ESP32 Dev Module 或者 ESP32-WROOM-DA Modulle
+
+<img width="143" alt="image" src="https://github.com/liyinchigithub/esp32-wroom-car/assets/19643260/9cdddaaa-69ad-4fd2-8f1b-37a29594c37f">
+
+
+<img width="914" alt="image" src="https://github.com/liyinchigithub/esp32-wroom-car/assets/19643260/1850680e-9e3c-4848-b45d-abac407317e7">
+
+#### 选择端口
+
+
+<img src="" width="200" height="150">
+
+#### 驱动安装
+
+<img width="200" height="150" alt="image" src="">
+
+
+
+### 二、（可变速）L287N直流电机（步进器电机驱动板）
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/4be41a85-daaa-47ba-a8ee-69594b922bd1" width="200" height="550">
+
+L298N电机驱动板通常需要四个控制引脚来驱动一个双电机：
+
+- 两个用于控制电机A的方向，另外两个用于控制电机B的方向。
+- 还有两个使能引脚（一个用于电机A，一个用于电机B），可以用来控制电机的速度，通常通过PWM信号实现。
+
+1. OUT1和OUT2 电池1、OUT3和OUT4 电池2
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/cc0a4d85-65d7-4f76-acab-0b359a90d4be" width="300" height="200">
+
+
+2. 12V电源正极（暂不用）、GND电源负极接到esp32电源负极 、+5V接到esp32电源正极极
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/c999d437-46b0-43eb-8c7f-5f245023c422" width="300" height="200">
+
+
+3. ENA：电机1PWM调速引脚
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/4fabed0e-b6d7-4cd6-9a88-b94c54a44d8c" width="300" height="200">
+
+**注意**：不使用调速引脚，那两个塑料不要拔起来，拔起来后需要用“母对母”跳线连接
+
+5. ENB：电机2PWM调速引脚
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/e572ce93-db48-47b2-bff0-e6e7f846b1cb" width="300" height="200">
+
+
+6. IN1和IN2：电机1 正反转/停止控制
+   
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/73b05a29-3d88-49ee-ac6e-4be1033e2765" width="300" height="200">
+
+7. IN3和IN4：电机2 正反转/停止控制
+
+* <img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/b9c65e78-46da-4abe-b596-874bfc079843" width="300" height="200">
+
+
+* <img width="800"  height="400" alt="image" src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/72d39314-f720-45dd-b862-290088d42a8e">
+
+* <img width="739" alt="image" src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/96386172-4779-4443-9e55-ea090757837c">
+
+
+8.左右电机控制
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/f0de3c2f-4cdc-4fba-9f51-eb6617a97c96" width="300" height="200">
+
+
+
+9.引脚对接
+
+（1）esp32
+
+| L298N引脚 | ESP32引脚 |
+|-----------|-----------|
+| IN1 | 23 |
+| IN2 | 22 |
+| IN3 | 19 |
+| IN4 | 18 |
+| ENA | 21 |
+| ENB | 5 |
+
+（2）esp8266
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/d2b2ce52-44fb-4903-a1cd-41f9e2f1b2eb" width="200" height="200">
+
+<img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/0c27b5c8-5c60-4ea2-aef6-1f0f54a0f2ec" width="200" height="200">
+
+
+
+[B站视频1](https://www.bilibili.com/video/BV1TW4y1y7PT/?spm_id_from=333.337.search-card.all.click&vd_source=73d9ae1d519db8c687b2d2e7b02c99ed)
+[B站视频2](https://www.bilibili.com/video/BV1Xt411372X/?spm_id_from=autoNext&vd_source=73d9ae1d519db8c687b2d2e7b02c99ed)
+
+
+
+
+
+### 三、（不可变速）L9110S直流电机（步进器电机驱动板）
+
+- <img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/4b6524c5-65e4-40f3-8112-f5a8f1a63fd4" width="300" height="300">
+
+
+- <img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/427a38aa-8058-4608-b2c3-3a224a8be586" width="300" height="300">
+
+
+- <img src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/63b7af22-43ba-44dd-9fcb-7d5651c6fffd" width="300" height="300">
+
+## 遥控按钮页面
+
+1.程序烧入ESP32主板后，生成wifi热点：“遥控小车”；
+2.使用手机或平板连接热点密码：12345678
+3.浏览器地址：http://192.168.4.1
+
+<img width="300" height="600" alt="image" src="https://github.com/liyinchigithub/esp8266_car_webServer/assets/19643260/7ca61931-389f-4d21-8b0d-e1590c4028b9">
+
+
+## 常见问题
+
+如果你已经将引脚设置为高电平，但是马达没有运动，可能有以下几个原因：
+
+1. 电流不足：ESP32的GPIO引脚只能提供很小的电流（大约12mA），这可能不足以驱动马达。可能需要一个马达驱动器或者一个适当的晶体管来放大电流。
+
+2. 连接问题：检查你的马达是否正确连接到ESP32和电源。
+
+3. 马达问题：你的马达可能有问题。试着用另一个马达，或者直接用电源给马达供电，看看马达是否能运动。使用专门的马达控制板（如L293D或者L298N）来驱动你的马达。
+
+4. 代码问题：检查你的代码是否正确设置了GPIO引脚为输出模式，并且设置了高电平。
+
+
+
+
+
---
+++ server/esp32_wroom_car_server/esp32_wroom_car_server.ino
@@ -0,0 +1,266 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+
+const char *ssid = "遥控小车";      // 你想要的热点名称
+const char *password = "12345678";  // 热点的密码，至少8个字符
+
+// 定义电机驱动板的引脚，ESP32的引脚
+const int motorLeftIn1 = 23;   // 左电机输入1 IN1
+const int motorLeftIn2 = 22;   // 左电机输入2 IN2
+const int motorRightIn1 = 19;  // 右电机输入1 IN3
+const int motorRightIn2 = 18;  // 右电机输入2 IN4
+const int motorLeftEn = 21;    // 左电机使能 ENA
+const int motorRightEn = 5;    // 右电机使能 ENB
+
+// 前端HTML页面内容定义为字符串
+const char *webpage PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>遥控小车V2.0 Remote Car Control</title>
+<style>
+<style>
+  body, html {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #control-panel {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #horizontal-controls {
+    display: flex;
+    justify-content: center;
+    margin: 20px; /* 为水平控制按钮组添加外边距 */
+  }
+  button {
+    font-size: 2em;
+    padding: 20px;
+    margin: 10px; /* 为所有按钮添加外边距 */
+  }
+  .red-button {
+    background-color: red;
+    color: white; /* 设置文字颜色为白色以提高对比度 */
+  }
+
+  .blue-button {
+    background-color: blue;
+    color: white; /* 设置文字颜色为白色以提高对比度 */
+  }
+
+  /* 你可能还想为按钮添加一些hover效果 */
+  .red-button:hover, .blue-button:hover {
+    opacity: 0.8; /* 鼠标悬停时的透明度变化 */
+  }
+</style>
+<script>
+// JavaScript代码
+function startCommand(action) {
+  // 发送开始运动的指令到Arduino
+  fetch('/start', {
+    method: 'POST',
+    body: JSON.stringify({ action: action }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
+function stopCommand() {
+  // 发送停止运动的指令到Arduino
+  fetch('/stop', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
+// 修改事件监听器来处理按下和松开
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    // 鼠标事件
+    button.addEventListener('mousedown', () => startCommand(button.dataset.action));
+    button.addEventListener('mouseup', stopCommand);
+    button.addEventListener('mouseleave', stopCommand);
+    // 触摸事件
+    button.addEventListener('touchstart', (e) => {
+      e.preventDefault(); // 阻止触摸时屏幕的滚动
+      startCommand(button.dataset.action);
+    });
+    button.addEventListener('touchend', stopCommand);
+  });
+});
+</script>
+</head>
+<body>
+<div id="control-panel">
+  <button id="forward" class="blue-button" data-action="3">前</button>
+  <div id="horizontal-controls">
+    <button id="left" class="blue-button" data-action="1">左</button>
+    <button id="right" class="blue-button" data-action="2">右</button>
+  </div>
+  <button id="backward" class="blue-button" data-action="5">后</button>
+</div>
+<!-- MJPEG流 -->
+// <div style="margin-top: 20px;">
+//   <img style="display: block; -webkit-user-select: none; margin: auto; background-color: hsl(0, 0%, 25%);" src="http://192.168.4.2/mjpeg/1">
+// </div>
+</body>
+</html>
+)rawliteral";
+WebServer server(80);
+
+// 函数原型声明
+void setMotorState(int action, bool continuous);
+void handleGet();
+void handlePost();
+
+// 停止所有电机
+void stopMotors() {
+  digitalWrite(motorLeftIn1, LOW);
+  digitalWrite(motorLeftIn2, LOW);
+  digitalWrite(motorRightIn1, LOW);
+  digitalWrite(motorRightIn2, LOW);
+}
+
+// 处理开始运动的请求
+void handleStart() {
+  if (server.method() == HTTP_POST) {
+    StaticJsonDocument<200> doc;
+    deserializeJson(doc, server.arg("plain"));
+    int action = doc["action"].as<int>();
+    setMotorState(action, true);  // 开始运动
+    server.send(200, "text/plain", "Start action received.");
+  }
+}
+
+// 处理停止运动的请求
+void handleStop() {
+  if (server.method() == HTTP_POST) {
+    stopMotors();  // 停止所有电机
+    server.send(200, "text/plain", "Stop action received.");
+  }
+}
+
+// 初始化
+void setup() {
+  Serial.begin(115200);
+  // 连接WiFi
+  // WiFi.begin(ssid, password);
+  // Serial.println("Attempting to connect to WiFi network...");
+
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.print(".");
+  // }
+
+  bool ssid_hidden = false; // 设置为true可以隐藏SSID
+  int channel = 1;          // 设置WiFi信道
+  int max_connection = 5;   // 设置最大连接数
+  // 设置ESP32为AP模式，使用上面定义的SSID和密码
+  bool result = WiFi.softAP(ssid, password, channel, ssid_hidden, max_connection);
+  Serial.println("ESP32 is set as a WiFi Access Point with SSID: " + String(ssid));
+
+  if(result) {
+    Serial.println("Access Point Started");
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+  } else {
+    Serial.println("Access Point Failed to Start");
+  }
+
+  // 设置电机驱动引脚为输出模式
+  pinMode(motorLeftIn1, OUTPUT);
+  pinMode(motorLeftIn2, OUTPUT);
+  pinMode(motorRightIn1, OUTPUT);
+  pinMode(motorRightIn2, OUTPUT);
+  pinMode(motorLeftEn, OUTPUT);
+  pinMode(motorRightEn, OUTPUT);
+
+  // 启用电机
+  digitalWrite(motorLeftEn, HIGH);
+  digitalWrite(motorRightEn, HIGH);
+
+  // 初始化Web服务器并设置主页响应
+  server.on("/", handleGet);
+  server.on("/command", handlePost);  // 短按命令
+  server.on("/start", handleStart);   // 开始运动的请求
+  server.on("/stop", handleStop);     // 停止运动的请求
+  server.begin();
+  Serial.println("Web server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+// 处理GET请求以返回HTML页面
+void handleGet() {
+  if (server.method() == HTTP_GET) {
+    server.send(200, "text/html", webpage);
+  }
+}
+
+// 处理POST请求以控制电机
+void handlePost() {
+  if (server.method() == HTTP_POST) {
+    StaticJsonDocument<200> doc;
+    deserializeJson(doc, server.arg("plain"));
+    int action = doc["action"].as<int>();
+    bool longPress = doc["longPress"].as<bool>();  // 长按标志
+
+    setMotorState(action, longPress);
+
+    if (!longPress) {
+      delay(1000);   // 短按持续1秒
+      stopMotors();  // 停止电机
+    }
+
+    server.send(200, "text/plain", "Action received.");
+  }
+}
+
+void setMotorState(int action, bool continuous) {
+  // 根据action设置电机状态
+  switch (action) {
+    case 1:  // 左转
+      digitalWrite(motorLeftIn1, HIGH);
+      digitalWrite(motorLeftIn2, LOW);  // 一个电机向前
+      digitalWrite(motorRightIn1, LOW);
+      digitalWrite(motorRightIn2, HIGH);  // 另一个电机向后
+      break;
+    case 2:  // 右转
+      digitalWrite(motorLeftIn1, LOW);
+      digitalWrite(motorLeftIn2, HIGH);  // 一个电机向后
+      digitalWrite(motorRightIn1, HIGH);
+      digitalWrite(motorRightIn2, LOW);  // 另一个电机向前
+      break;
+    case 3:  // 前进
+      digitalWrite(motorLeftIn1, HIGH);
+      digitalWrite(motorLeftIn2, LOW);  // 两个电机都向前
+      digitalWrite(motorRightIn1, HIGH);
+      digitalWrite(motorRightIn2, LOW);
+      break;
+    case 5:  // 后退
+      digitalWrite(motorLeftIn1, LOW);
+      digitalWrite(motorLeftIn2, HIGH);  // 两个电机都向后
+      digitalWrite(motorRightIn1, LOW);
+      digitalWrite(motorRightIn2, HIGH);
+      break;
+    default:
+      // 如果收到的action不是预期的，停止所有电机
+      stopMotors();
+      break;
+  }
+  // 如果不是长按，不需要在这里停止电机，因为会有单独的停止请求
+}
